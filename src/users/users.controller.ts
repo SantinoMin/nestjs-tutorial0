6b9240cd2415ec26216bc0d509fb5a50f50c56ec
Sangmin <mannone@rangeduck.com>
2023-06-02 06:29:56 +0200

    update 0602 0630 last

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,34 +20,34 @@ import { ApiTags } from '@nestjs/swagger';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  getList(): User[] {
-    return this.usersService.findAll();
-  }
+  // @Get()
+  // getList(): User[] {
+  //   return this.usersService.findAll();
+  // }
 
-  @Get(':user_idx')
-  findId(
-    @Param('user_idx', new ParseIntPipe())
-    user_idx: number,
-  ): string {
-    console.log('id : ', user_idx);
-    // get by ID logic
-    return this.usersService.findId(user_idx);
-  }
+  // @Get(':user_idx')
+  // findId(
+  //   @Param('user_idx', new ParseIntPipe())
+  //   user_idx: number,
+  // ): string {
+  //   console.log('id : ', user_idx);
+  //   // get by ID logic
+  //   return this.usersService.findId(user_idx);
+  // }
 
-  @Post('signup')
-  signup1(@Body() createUserDto: CreateUserDto): User {
-    const { username, password } = createUserDto;
-    return this.usersService.signup(createUserDto);
-  }
+  // @Post('signup')
+  // signup1(@Body() createUserDto: CreateUserDto): User {
+  //   const { username, password } = createUserDto;
+  //   return this.usersService.signup(createUserDto);
+  // }
 
-  @Post('signin')
-  signin1(@Body() createUserDto: CreateUserDto): boolean {
-    return this.usersService.signin(createUserDto);
-  }
+  // @Post('signin')
+  // signin1(@Body() createUserDto: CreateUserDto): boolean {
+  //   return this.usersService.signin(createUserDto);
+  // }
 
-  @Patch()
-  editUser(@Body() updateUserDto: UpdateUserDto): any {
-    return this.usersService.updateUser(updateUserDto);
-  }
+  // @Patch()
+  // editUser(@Body() updateUserDto: UpdateUserDto): any {
+  //   return this.usersService.updateUser(updateUserDto);
+  // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,70 +1,91 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable } from '@nestjs/common';
-import { User } from './interface/user.interface';
+// import { User } from './interface/user.interface';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/users.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
 
-  signup(createUserDto: CreateUserDto): User {
-    const { username, password } = createUserDto;
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hash = bcrypt.hashSync(password, salt);
-
-    const user: User = {
-      username: username,
-      password: hash,
-      salt: salt,
-    };
-    this.users.push(user);
-    return user;
+  // find()메소드 =
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
-  signin(createUserDto: CreateUserDto): boolean {
-    const { username, password } = createUserDto;
-    let authCheck = false;
-    this.users.forEach((item) => {
-      const hash = bcrypt.hashSync(password, item.salt);
-
-      if (item.username == username && item.password == password) {
-        authCheck = true;
-      }
-    });
-    return authCheck;
+  findOne(id: number): Promise<User> {
+    return this.usersRepository.findOneBy({ id });
   }
 
-  findId(user_idx: number): string {
-    console.log('length', this.users.length);
-    if (this.users.length > user_idx) {
-      return this.users[user_idx].username;
-    } else {
-      return 'User not found';
-    }
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
   }
 
-  findAll(): User[] {
-    return this.users;
-  }
+  // private readonly users: User[] = [];
 
-  updateUser(updateUserDto: UpdateUserDto): any {
-    const { user_idx, username, password } = updateUserDto;
-    if (this.users.length > parseInt(user_idx)) {
-      const saltRounds = 10;
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hash = bcrypt.hashSync(password, salt);
+  // signup(createUserDto: CreateUserDto): User {
+  //   const { username, password } = createUserDto;
+  //   const saltRounds = 10;
+  //   const salt = bcrypt.genSaltSync(saltRounds);
+  //   const hash = bcrypt.hashSync(password, salt);
 
-      const user: User = {
-        username: username,
-        password: hash,
-        salt: salt,
-      };
-      this.users[user_idx] = user;
-      return user;
-    } else {
-      return 'User not found';
-    }
-  }
+  //   const user: User = {
+  //     username: username,
+  //     password: hash,
+  //     salt: salt,
+  //   };
+  //   this.users.push(user);
+  //   return user;
+  // }
+
+  // signin(createUserDto: CreateUserDto): boolean {
+  //   const { username, password } = createUserDto;
+  //   let authCheck = false;
+  //   this.users.forEach((item) => {
+  //     const hash = bcrypt.hashSync(password, item.salt);
+
+  //     if (item.username == username && item.password == password) {
+  //       authCheck = true;
+  //     }
+  //   });
+  //   return authCheck;
+  // }
+
+  // findId(user_idx: number): string {
+  //   console.log('length', this.users.length);
+  //   if (this.users.length > user_idx) {
+  //     return this.users[user_idx].username;
+  //   } else {
+  //     return 'User not found';
+  //   }
+  // }
+
+  // findAll(): User[] {
+  //   return this.users;
+  // }
+
+  // updateUser(updateUserDto: UpdateUserDto): any {
+  //   const { user_idx, username, password } = updateUserDto;
+  //   if (this.users.length > parseInt(user_idx)) {
+  //     const saltRounds = 10;
+  //     const salt = bcrypt.genSaltSync(saltRounds);
+  //     const hash = bcrypt.hashSync(password, salt);
+
+  //     const user: User = {
+  //       username: username,
+  //       password: hash,
+  //       salt: salt,
+  //     };
+  //     this.users[user_idx] = user;
+  //     return user;
+  //   } else {
+  //     return 'User not found';
+  //   }
+  // }
 }
